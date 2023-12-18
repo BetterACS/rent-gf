11e998c03b72c43fdfb9ba9dfd7e95182cb17cf7
BankDesu <psuwimonopas@gmail.com>
2023-12-19 05:01:36 +0700

    delete

--- rentaldadad/path/cookie.js
+++
@@ -1,9 +0,0 @@
-const cookieSession = require('cookie-session');
-
-const sessionMiddleware = cookieSession({
-  name: 'session',
-  keys: ['your-secret-key'],
-  maxAge: 24 * 60 * 60 * 1000, // 24 hours
-});
-
-module.exports = sessionMiddleware;
