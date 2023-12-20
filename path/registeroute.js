2aae3d595416ee4ba8c7c66f3dcf11f1c6eed808
BankDesu <psuwimonopas@gmail.com>
2023-12-20 13:29:42 +0700

    register

--- path/registeroute.js
+++ path/registeroute.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const multer = require('multer');
 const upload = multer();
 const dbConnection = require('../database');
-const bcrypt = require('bcrypt');
+
 
 router.get('/signup', (req, res) => {
     // Send the HTML file
@@ -12,38 +12,33 @@ router.get('/signup', (req, res) => {
     res.sendFile(filePath);
 });
 
-router.post('/signup', upload.none(), async (req, res) => {
+router.post('/signup', upload.none(), (req, res) => {
     console.log(req.body);
     const { email, username, tel, age, gender, password } = req.body;
 
-    try {
-        const hashedPassword = await bcrypt.hash(password, 10);
+    dbConnection.query('SELECT email FROM User WHERE email = ?', [email], (err, results) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).json({ error: 'Internal Server Error' });
+        }
 
-        dbConnection.query('SELECT email FROM User WHERE email = ?', [email], (err, results) => {
-            if (err) {
-                console.error(err);
-                return res.status(500).json({ error: 'Internal Server Error' });
-            }
+        if (results.length > 0) {
+            return res.status(400).json({ error: 'Email address is already in use.' });
+        } else {
+            dbConnection.query('INSERT INTO User (email, username, passwords, tel, gender , age) VALUES (?, ?, ? ,?, ?, ?)',
+                [email, username, password, tel, gender, age],
+                (err, results) => {
+                    if (err) {
+                        console.error(err);
+                        return res.status(500).json({ error: 'Internal Server Error' });
+                    }
 
-            if (results.length > 0) {
-                return res.status(400).json({ error: 'Email address is already in use.' });
-            } else {
-                dbConnection.query('INSERT INTO User (email, username, passwords, tel, gender , age) VALUES (?, ?, ? ,?, ?, ?)',
-                    [email, username, hashedPassword, tel, gender, age],
-                    (err, results) => {
-                        if (err) {
-                            console.error(err);
-                            return res.status(500).json({ error: 'Internal Server Error' });
-                        }
-
-                        return res.status(200).json({ message: 'Registration successful. Redirecting to login page.' });
-                    });
-            }
-        });
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({ error: 'Internal Server Error' });
-    }
+                    return res.status(200).json({ message: 'Registration successful. Redirecting to login page.' });
+                });
+        }
+    });
 });
+
+
 
 module.exports = router;
